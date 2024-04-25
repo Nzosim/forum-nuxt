51e87c75d0557bbdc82042586d22626fad540e06
ESN19 <ESN19@3ds.com>
2024-04-25 11:23:25 +0200

    nuxt sous docker

--- server/sql/dbConnection.ts
+++ server/sql/dbConnection.ts
@@ -1,14 +1,14 @@
-import mysql from 'mysql2/promise'
-import bluebird from 'bluebird'
+import mysql from "mysql2/promise";
+import bluebird from "bluebird";
 
 export async function getConnection() {
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'projet',
-        password: 'projet',
-        database: 'projet',
-        Promise: bluebird,
-    })
+  const connection = await mysql.createConnection({
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "projet",
+    Promise: bluebird,
+  });
 
-    return connection
+  return connection;
 }
